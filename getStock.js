fbd00d6006dd45208be22593a3e76caa86359264
Lu Yi-Te <jikotw74@gmail.com>
2017-10-13 23:01:17 +0800

    feat: getStockInfo

--- getStock.js
+++ getStock.js
@@ -1,50 +1,80 @@
 const puppeteer = require('puppeteer');
 let browser = false;
 
-async function getStockInfo(key){
+async function getStockInfo(keys){
+    console.log('getStockInfo', keys);
+
     let now = new Date();
     const page = await browser.newPage();
     await page.goto(
-        `http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=${key}&json=1&delay=0&_=${now.getTime()}`, 
+        `http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=${keys}&json=1&delay=0&_=${now.getTime()}`, 
         {waitUntil: 'networkidle'}
     );
     const bodyHandle = await page.$('body');
     const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+    await page.close();
     return JSON.parse(html);
 }
 
 async function getStock(stock_id){
-    browser = await puppeteer.launch({
-        // headless: false,
-        // slowMo: 250 // slow down by 250ms
-    });
-
-    let now = new Date();
-    let results = [];
-
-    // get cookies
-    const page = await browser.newPage();
-    await page.goto(`http://mis.twse.com.tw/stock/fibest.jsp?lang=zh_tw&stock=${stock_id}`);
-    // const cookies = await page.cookies();
-    // console.log(cookies);
+    // console.log('[getStock id] ', stock_id);
 
     now = new Date();
-    const page2 = await browser.newPage();
-    await page2.goto(`http://mis.twse.com.tw/stock/api/getStock.jsp?ch=${stock_id}.tw&json=1&_=${now.getTime()}`);
-    const bodyHandle = await page2.$('body');
-    const html = await page2.evaluate(body => body.innerHTML, bodyHandle);
+    const page = await browser.newPage();
+    await page.goto(`http://mis.twse.com.tw/stock/api/getStock.jsp?ch=${stock_id}.tw&json=1&_=${now.getTime()}`);
+    const bodyHandle = await page.$('body');
+    const html = await page.evaluate(body => body.innerHTML, bodyHandle);
     const res = JSON.parse(html);
+    await page.close();
+    return res;
+}
 
-    if(res && res.rtcode === '0000'){
-        await Promise.all(res.msgArray.map(async (item) => {
-            const info = await getStockInfo(item.key);
-            results.push(info);
+async function getStocks(stock_ids){
+    // console.log('[getStocks ids] ', stock_ids);
+    let result = false;
+
+    stock_ids = [].concat(stock_ids);
+    if(stock_ids.length > 0){
+        let queryKeys = [];
+
+        // start brwoser
+        browser = await puppeteer.launch({
+            // headless: false,
+            // slowMo: 250 // slow down by 250ms
+        });
+
+        // get cookies
+        const page = await browser.newPage();
+        await page.goto(`http://mis.twse.com.tw/stock/fibest.jsp?lang=zh_tw&stock=${stock_ids[0]}`);
+        // const cookies = await page.cookies();
+        // console.log(cookies);
+
+        // get query keys
+        await Promise.all(stock_ids.map(async (id) => {
+            const res = await getStock(id);
+            if(res && res.rtcode === '0000'){
+                if(res.msgArray && res.msgArray[0] && res.msgArray[0].key){
+                    // console.log(res.msgArray[0].key);
+                    queryKeys.push(res.msgArray[0].key);    
+                }
+            }
         }));
+
+        // console.log('[getStocks queryKeys] ', queryKeys);
+
+        if(queryKeys.length > 0){
+            const keys = queryKeys.join('|');
+            result = await getStockInfo(keys);
+        }
     }
 
     await browser.close();
-    console.log('getStock', results);
-    return results;
+    // console.log('[getStocks return] ', result);
+    return result;
 }
 
-module.exports = getStock;
+module.exports = getStocks;
+
+// Promise.all([getStocks(['2327', '2890', '6180'])])
+// .then(message => console.log(message))
+// .catch(error => console.log(error))
--- ioServer.js
+++ ioServer.js
@@ -1,23 +1,67 @@
-const getStock = require('./getStock');
+const getStocks = require('./getStock');
 
 const port = process.env.PORT || 3001;
+let STOCKS = {};
 
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
-
 io.on('connection', function(client){
+	let intervalID = false;
+	let lastUpdatedTime = 0;
 	console.log('connection');
-  	client.on('event', function(data){});
-  	client.on('test', function(data){
-  		console.log(data);
-  		client.emit('test_response', data + (new Date().getTime()));
-  		const sendStock = async () => {
-  			const stock = await getStock('6180');
-  			client.emit('test_response', stock); 
+  	client.on('registStocks', stock_ids => {
+  		let isInit = false;
+  		// console.log('registStocks', stock_ids)
+  		stock_ids.forEach(id => {
+  			if(!STOCKS[id]){
+  				STOCKS[id] = {
+  					id: id,
+  					updatedTime: false
+  				}
+  			}
+  		});
+
+  		// console.log(STOCKS);
+
+  		if(!intervalID){
+  			intervalID = setInterval(async () => {
+  				console.log(`interval start ${intervalID}`);
+
+  				const now = new Date();
+  				const nowH = now.getHours();
+  				const isRealTimeMode = nowH >= 8 && nowH <= 12;
+
+  				if(!isInit || isRealTimeMode){
+  					let lastTime = 0;
+					const response = await getStocks(Object.keys(STOCKS));  
+					response.msgArray.forEach(msg => {
+				  		STOCKS[msg.c] = {
+				  			...STOCKS[msg.c],
+				  			updatedTime: msg.tlong,
+				  			openPrice: msg.o,
+				  			nowPrice: msg.z
+				  		}
+				  		if(msg.tlong > lastTime){
+				  			lastTime = msg.tlong;
+				  		}
+				  	});		
+				  	if(lastTime > lastUpdatedTime){
+				  		lastUpdatedTime = lastTime;
+				  		client.emit('updateStocks', STOCKS);	
+				  		isInit = true;
+				  	}			
+  				}
+			}, 10000)
   		}
-  		sendStock();
   	});
+  	// client.on('getStockPrice', async (stock_ids) => {
+  	// 	console.log(stock_ids);
+  	// 	// client.emit('test_response', data + (new Date().getTime()));
+  	// 	const respose = await getStocks(stock_ids);
+
+  	// 	client.emit('updateStockPrice', respose);
+  	// });
   	client.on('disconnect', function(){});
 });
 server.listen(port, () => {
